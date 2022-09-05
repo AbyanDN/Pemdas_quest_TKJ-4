a67c29798150f2b83605fd9d8674f6cb1f71cb1a
AbyanDN <112526785+AbyanDN@users.noreply.github.com>
2022-09-05 10:08:43 +0700

    Add files via upload

---
+++ Python6- TKJ4. Py.py
@@ -0,0 +1,49 @@
+# list kosong
+list_kosong = [abyan]
+
+# List yang berisi kumpulan string
+list_buah = ["pisang", "Nanas", "Melon", "Durian"]
+
+
+# List yang berisi kumpulan integer
+list_nilai = [80, 70, 90, 60]
+
+# List campuran berbagai tipe data
+list_jawaban = [150, 33.33, 'Presiden Sukarno', False]
+
+Menampilkanlist
+
+print('list_kosong:', list_kosong)
+print('list_buah:', list_buah)
+print('list_nilai:', list_nilai)
+print('list_jawaban:', list_jawaban)
+
+
+lisr_kosong: []
+list_buah: ["Pisang", "Nanas", "Melon", "Durian"]
+list_nilai: [80, 70, 90, 60]
+list_jawaban: [150, 33.33, 'Presiden Sukarno', False]
+
+print(list_buah[0])
+print(list_buah[2])
+print(list_buah[1])
+print(list_buah[3])
+
+
+Pisang
+Melon
+Nanas
+Durian
+
+print(list_buah[-1])
+print(list_buah[-2])
+print(list_buah[-3])
+print(list_buah[-4])
+
+Durian
+Melon
+Nanas
+Pisang
+
+
+      
